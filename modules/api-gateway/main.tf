88f58a3de85f9dfeaec7c1b7bc7c94a5c45f2073
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-04-07 13:40:40 +0530

    Update main.tf

--- modules/api-gateway/main.tf
+++ modules/api-gateway/main.tf
@@ -23,16 +23,16 @@ resource "aws_api_gateway_method" "method" {
   authorization   = each.value.authorization
 }
 
-# resource "aws_api_gateway_integration" "integration" {
-#   for_each = var.integration
+ resource "aws_api_gateway_integration" "integration" {
+   for_each = var.integration
 
-#   rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
-#   resource_id             = aws_api_gateway_resource.resource[each.value.resource_name].id
-#   http_method             = aws_api_gateway_method.method[each.value.http_method]
-#   integration_http_method = each.value.http_method
-#   type                    = "AWS_PROXY"
-#   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${each.value.lambda_function_arn}/invocations"
-# }
+   rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
+   resource_id             = aws_api_gateway_resource.resource[each.value.resource_name].id
+   http_method             = aws_api_gateway_method.method[each.value.http_method]
+   integration_http_method = each.value.http_method
+   type                    = "AWS_PROXY"
+   uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${each.value.lambda_function_arn}/invocations"
+ }
 
 # resource "aws_lambda_permission" "allow_api" {
 #   for_each = var.lambda_permission 
